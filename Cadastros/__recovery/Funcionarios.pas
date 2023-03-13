52a750787b6ae18ede3cc1e36d5f36de1621b749
Arthur Benfica Graff <arthur_benfica@hotmail.com>
2023-03-13 14:48:15 -0300

    Initial commit

---
+++ Cadastros/__recovery/Funcionarios.pas
@@ -0,0 +1,24 @@
+unit Funcionarios;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmFuncionarios = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmFuncionarios: TFrmFuncionarios;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ __recovery/Menu.pas
@@ -0,0 +1,43 @@
+unit Menu;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TFrmMenu = class(TForm)
+    MainMenu1: TMainMenu;
+    Cadastros1: TMenuItem;
+    Produtos1: TMenuItem;
+    Fornecedores1: TMenuItem;
+    Clientes1: TMenuItem;
+    Estoque1: TMenuItem;
+    Movimentaes1: TMenuItem;
+    Relatrios1: TMenuItem;
+    Sair1: TMenuItem;
+    Usurios1: TMenuItem;
+    procedure Usurios1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmMenu: TFrmMenu;
+
+implementation
+
+{$R *.dfm}
+
+uses Usuarios;
+
+procedure TFrmMenu.Usurios1Click(Sender: TObject);
+begin
+FrmUsuarios := TFrmUsuarios.Create(self);
+FrmUsuarios.ShowModal;
+end;
+
+end.
